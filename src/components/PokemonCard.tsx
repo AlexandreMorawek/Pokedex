69dfcd9c53919baae21b42210e8299491e04db6b
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-03-31 16:29:36 +0200

    Contenu dynamique

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,24 @@
-function MyTitle() {
-    return <figure>
-        <img
-            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        />
-            <figcaption>Bulbasaur</figcaption>
-    </figure>;
+  const pokemonList = [
+    {
+      name: "Bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+        alt: "Bulbasaur",
+    },
+    {
+      name: "Mew",
+    },
+  ];
 
+function PokemonCard() {
+    const pokemon = pokemonList[1];
+    return <>
+        {pokemon.name}<br></br>
+        {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc}/>}         
+        </>;
   }
   
-  export default MyTitle;
+  export default PokemonCard;
+
+  
+  
