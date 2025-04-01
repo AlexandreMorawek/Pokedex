67918e8cbb3e93f17ce0d485a061087537ab7c8c
AlexandreMorawek <alexandre.morawek@gmail.com>
2025-04-01 15:32:36 +0200

    changing card to dynamic

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,17 +1,6 @@
-  const pokemonList = [
-    {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        alt: "Bulbasaur",
-    },
-    {
-      name: "Mew",
-    },
-  ];
+ 
+function PokemonCard({pokemon}) {
 
-function PokemonCard() {
-    const pokemon = pokemonList[0];
     return <>
         {pokemon.name}<br></br>
         {pokemon.imgSrc === undefined ? <p>???</p> : <img src={pokemon.imgSrc}/>}         
